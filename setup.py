89d72b05b4519f7cf9c612e78a903eab620268c0
Bhaskar Tripathi <35177508+bhaskatripathi@users.noreply.github.com>
2024-01-22 17:55:50 +0530

    Clean up repository

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+# read contents of README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file(os.path.join(this_directory, 'README.md'), 'rst')
+except(IOError, ImportError):
+    with codecs.open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
 
 setup(
     name='pyviralcontent',
@@ -13,5 +25,9 @@ setup(
     author='Bhaskar Tripathi',
     author_email='bhaskar.tripathi@gmail.com',
     description='A package for analyzing content readability and virality potential.',
-    keywords='readability virality content-analysis'
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    keywords='readability virality content-analysis',
+    url='https://github.com/bhaskatripathi/pyviralcontent',
+    # Additional metadata
 )
